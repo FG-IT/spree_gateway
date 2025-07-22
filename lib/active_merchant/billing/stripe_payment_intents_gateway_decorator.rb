6987d9def41a83cd2d760a773ed11a6e86fb0b1b
Y Z <plpm.zhao@gmail.com>
2025-07-22 09:48:30 -0400

    add statement descriptor

--- lib/active_merchant/billing/stripe_payment_intents_gateway_decorator.rb
+++ lib/active_merchant/billing/stripe_payment_intents_gateway_decorator.rb
@@ -2,10 +2,23 @@ module ActiveMerchant
   module Billing
     module StripePaymentIntentsGatewayDecorator
 
+      # set_automatic_payment_methods and add_dynamic_statement_descriptor are not related to this method
+      # however, in order to make things simple, it's ok to put them here.
+      # https://github.com/activemerchant/active_merchant/blob/v1.126.0/lib/active_merchant/billing/gateways/stripe_payment_intents.rb#L311
+      #
       def extract_token_from_string_and_maybe_add_customer_id(post, payment_method)
         if payment_method.starts_with?('ctoken_')
           post[:confirmation_token] = payment_method
         end
+        set_automatic_payment_methods(post)
+        add_dynamic_statement_descriptor(post)
+      end
+
+      def add_dynamic_statement_descriptor(post)
+        post[:statement_descriptor_suffix] = [*('A'..'Z'), *('a'..'z')].sample + SecureRandom.alphanumeric(7)
+      end
+
+      def set_automatic_payment_methods(post)
         post[:automatic_payment_methods] = {
           enabled: true,
           allow_redirects: 'never'
